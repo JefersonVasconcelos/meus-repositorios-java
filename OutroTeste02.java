f01b9c540ea8bf6dadd550db725d4516e1415e4a
JefersonVasconcelos <vasconcelos.jeferson@gmail.com>
2021-10-23 20:42:38 -0300

    exemplo 02 java

---
+++ OutroTeste02.java
@@ -0,0 +1,17 @@
+
+/**
+ * Escreva uma descrição da classe OutroTeste02 aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class OutroTeste02
+{
+    public static void main (String x[]) {
+        String saida = "";
+        
+        saida = x[0] + " - " + x [1];
+        
+        System.out.println(saida);
+    }
+}
---
+++ Teste2.java
@@ -0,0 +1,23 @@
+
+/**
+ * Escreva uma descrição da classe Teste2 aqui.
+ * 
+ * @author (Jeferson Vasconcelos) 
+ * @version (1.0  /  Data: 07/10/2021)
+ */
+public class Teste2
+{
+    public static void main (String args[])
+        {
+            //variavel com texto de saida
+            String saida;
+            
+            saida = "Esse é um teste do segundo exemplo";
+            System.out.println(saida);
+        }
+}
+    
+   
+
+
+    
