32480d295bba90abb5c8d105b2f444d674404657
JefersonVasconcelos <vasconcelos.jeferson@gmail.com>
2021-10-23 21:10:56 -0300

    Teste03

---
+++ Teste03.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jeferson
+ */
+public class Teste03 {
+    
+    public static void main (String args[]) 
+    { String saida = " Olá Mundo 03";
+    
+    System.out.println(saida);
+    }
+}
